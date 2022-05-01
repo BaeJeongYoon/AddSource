6bb040ec6cc55e3d83dac01099886c9cee092dbc
BaeJeongYoon <104703275+BaeJeongYoon@users.noreply.github.com>
2022-05-01 12:26:04 +0900

    Add files via upload

---
+++ tea_more.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    "id":"1",
+    "title":"유자 캐모마일",
+    "price":6100,
+    "image":"https://admin.hollys.co.kr/upload/menu/etc/menuEtc_201910240241538460.png",
+    "text":"은은한 향의 캐모마일과 유자차를 같이 느낄 수 있는 티베리에이션 음료"
+  },
+  {
+    "id":"2",
+    "title":"홍자몽차",
+    "price":5700,
+    "image":"https://admin.hollys.co.kr/upload/menu/etc/menuEtc_201601080256288770.png",
+    "text":"자몽과일 특유의 달콤 쌉싸름한 맛을 즐길 수 있는 과일차"
+  },
+  {
+    "id":"3",
+    "title":"복숭아 얼그레이",
+    "price":6500,
+    "image":"https://admin.hollys.co.kr/upload/menu/etc/menuEtc_201910240242172340.png",
+    "text":"달콤한 복숭아와 얼그레이의 베르가못 향의 조화, 깔끔하면서도 달콤한 티베리에이션 음료"
+  },
+  {
+    "id":"4",
+    "title":"해남 녹차",
+    "price":5100,
+    "image":"https://admin.hollys.co.kr/upload/menu/etc/menuEtc_201704140124051440.png",
+    "text":"고소한 감칠맛과 부드러운 목넘김이 좋은 땅끝 마음 해남에서 재배한 녹차(유기농 녹차잎 사용)"
+  },
+]
+ 
